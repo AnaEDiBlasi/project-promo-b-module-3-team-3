052b8d404ff00dd80fbeb880a8bf31c25ca466cc
Ana Elisa <elisaibz92@gmail.com>
2025-01-22 12:47:05 +0100

    completed proyect structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
-  //import React from 'react'
-  //import ReactDOM from 'react-dom/client'
-  //import App from './components/App.jsx'
 
-  import { StrictMode } from 'react'
+
+ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>)
   
 
- // ReactDOM.createRoot(document.getElementById('root')).render(
-   // <React.StrictMode>
-    //  <App />
-   // </React.StrictMode>,
-  //)
+ 
